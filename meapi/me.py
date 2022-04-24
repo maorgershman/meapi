d07e1674868838204bf26f5394e403cec6730430
David Lev <davidlev050414@gmail.com>
2022-04-24 19:35:39 +0300

    [me.py] handle access-token mode, save config_file to called dir

--- meapi/me.py
+++ meapi/me.py
@@ -1,6 +1,6 @@
-from os import path, sep
 from meapi.account import Account
 from meapi.auth import Auth
+from meapi.exceptions import MeException
 from meapi.notifications import Notifications
 from meapi.settings import Settings
 from meapi.social import Social
@@ -9,20 +9,26 @@ from meapi.util import Util
 
 class Me(Auth, Account, Social, Settings, Notifications, Util):
     def __init__(self,
-                 phone_number: int,
+                 phone_number: int = None,
                  access_token: str = None,
                  uuid: str = None,
-                 config_file: str = None,
+                 config_file: str = 'config.json',
                  proxies: dict = None):
 
-        if not config_file or not config_file.endswith(".json"):
-            self.config_file = 'config.json'
-        else:
+        if config_file.endswith(".json"):
             self.config_file = config_file
+        else:
+            print("Not a valid config json file. Using default 'config.json' file.")
+            self.config_file = 'config.json'
+
+        if not access_token and not phone_number:
+            raise MeException("You need to provide phone number or access token!")
+        if access_token and phone_number:
+            raise MeException("Access-token mode does not accept phone number, just access token.")
 
         self.access_token = access_token
         self.uuid = uuid
-        self.phone_number = self.valid_phone_number(phone_number)
+        self.phone_number = self.valid_phone_number(phone_number) if phone_number else phone_number
         self.proxies = proxies
 
         if not self.access_token:
